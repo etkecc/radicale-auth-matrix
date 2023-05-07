467d25275419d47855f322d9c91c302e644939fc
Aine <aine@etke.cc>
2023-05-07 23:52:19 +0300

    cache hashed credentials to avoid unnecessary calls to backend every time

--- radicale_auth_matrix/__init__.py
+++ radicale_auth_matrix/__init__.py
@@ -3,6 +3,7 @@ Authentication backend that checks credentials with a matrix server.
 """
 
 import json
+import hashlib
 import urllib
 import urllib.request
 from radicale import auth, config
@@ -10,10 +11,12 @@ from radicale import auth, config
 
 class Auth(auth.BaseAuth):
     _server: str
+    _cache: dict
 
     def __init__(self, configuration: config.Configuration) -> None:
         super().__init__(configuration)
         self._server = configuration.get("auth", "matrix_server")
+        self._cache = {}
 
     def _loginURL(self) -> str:
         return "{}/_matrix/client/v3/login".format(self._server)
@@ -42,7 +45,19 @@ class Auth(auth.BaseAuth):
         except urllib.error.HTTPError as e:
             print(e)
 
+    def _loggedInHash(self, login: str, password: str) -> str:
+        return hashlib.sha256("{} {}".format(login, password).encode("utf-8")).hexdigest()
+
+    def _loggedIn(self, login: str, password: str) -> None:
+        self._cache[self._loggedInHash(login, password)] = True
+
+    def _isLoggedIn(self, login: str, password: str) -> bool:
+        return self._loggedInHash(login, password) in self._cache.keys()
+
     def login(self, login: str, password: str) -> str:
+        if self._isLoggedIn(login, password):
+            return login
+
         req = urllib.request.Request(
             self._loginURL(),
             data=self._payload(login, password),
@@ -53,6 +68,7 @@ class Auth(auth.BaseAuth):
             resp = urllib.request.urlopen(req)
             data = json.loads(resp.read().decode("utf-8"))
             self._logout(data['access_token'])
+            self._loggedIn(login, password)
             return login
         except urllib.error.HTTPError as e:
             return ""
